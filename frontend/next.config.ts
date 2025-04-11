f2060baf4222704f068e55844ff8d2e448bb4f5e
Gaks3 <dwi66116@gmail.com>
2025-04-11 15:26:41 +0700

    initial commit

---
+++ frontend/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+        port: '3001',
+        protocol: 'http',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
