e5a376116735aec42148b4813a3b5ed2b7043c84
Gaks3 <dwi66116@gmail.com>
2025-04-11 15:33:43 +0700

    feat: create sidebar items

---
+++ frontend/src/components/items-developer-sidebar.tsx
@@ -0,0 +1,14 @@
+import { GamepadIcon, HomeIcon } from 'lucide-react';
+
+export const items = [
+  {
+    title: 'Home',
+    url: '/developer',
+    icon: HomeIcon,
+  },
+  {
+    title: 'Games',
+    url: '/developer/games',
+    icon: GamepadIcon,
+  },
+];
