1bc12255bb058e31fd9917a1615835809f0c13d6
Gaks3 <dwi66116@gmail.com>
2025-04-14 07:41:09 +0700

    feat: add users and games route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,29 +4,30 @@ import fs from 'fs-extra';
 
 import createApp from './lib/create-app.js';
 import { auth } from './lib/auth.js';
-import users from './routes/users/users.router.js';
-import games from './routes/games/games.router.js';
+import configureOpenAPI from './lib/configure-api.js';
+import users from './routes/users/users.index.js';
+import games from './routes/games/games.index.js';
 
-const app = createApp()
-  .use(
-    '*',
-    serveStatic({
-      root: './public/',
-      getContent: async (path) => {
-        try {
-          return await fs.readFile(path);
-        } catch (error) {
-          console.error('Failed to read file:', error);
-          return null;
-        }
-      },
-    })
-  )
-  .basePath('/api')
-  .route('/users', users)
-  .route('/games', games);
+const app = createApp().route('/', users).route('/', games);
 
-app.on(['POST', 'GET'], '/auth/*', (c) => auth.handler(c.req.raw));
+configureOpenAPI(app);
+
+app.use(
+  '*',
+  serveStatic({
+    root: './public/',
+    getContent: async (path) => {
+      try {
+        return await fs.readFile(path);
+      } catch (error) {
+        console.error('Failed to read file:', error);
+        return null;
+      }
+    },
+  })
+);
+
+app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw));
 
 export type AppType = typeof app;
 
